bdc9fc5e55f6e1e04cd541fb8307a6149ea76b69
Heider <heiderlopez123@gmail.com>
2022-08-02 16:13:11 -0500

    modelos (modelos genero y libros, por que no estaban echo)

--- Api/src/models/Genre.ts
+++
@@ -1,4 +0,0 @@
-import { Table, Model } from "sequelize-typescript"
-
-@Table
-export class Genre extends Model{}
--- Api/src/models/Libros.ts
+++ Api/src/models/Libros.ts
@@ -1,1 +1,37 @@
-console.log('puto el que lo lea')
+import { Table, Model, Column, DataType } from "sequelize-typescript";
+
+@Table
+export class Libros extends Model {
+    @Column
+    name!: string;
+
+    @Column
+    author!: string;
+
+    @Column
+    category!: string;
+
+    @Column
+    pages!: number;
+
+    @Column
+    publisher!: string;
+
+    @Column(DataType.STRING(500))
+    description!: string;
+
+    @Column
+    image!: string;
+
+    @Column(DataType.ENUM('0', '1', '2', '3', '4', '5'))
+    rating!: string;
+
+    @Column
+    price!: string;
+
+    @Column
+    released!: string;
+
+    @Column
+    language!: string
+}
