9001cd3060752808e14324d58202349f15245893
Heider <heiderlopez123@gmail.com>
2022-08-03 13:54:19 -0500

    modelo/user(crear el modelo y relacionarlo con carritocompras)

--- src/models/CarritoCompras.ts
+++ src/models/CarritoCompras.ts
@@ -1,8 +1,16 @@
-import { Table, Model, Column, DataType } from "sequelize-typescript";
+import { Table, Model, Column, ForeignKey, BelongsTo } from "sequelize-typescript";
 import { iCarritoCompras } from "../types/CarritoCompras";
+import { Usuario } from "./Usuario";
 
 @Table
 export class CarritoCompras extends Model<iCarritoCompras>{
     @Column
     items!: string[]
+
+    @ForeignKey(() => Usuario)
+    @Column
+    userid!: number
+
+    @BelongsTo(() => Usuario)
+    Usuario!: Usuario
 }
