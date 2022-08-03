19106a300a9c8f382ec357d1387706ada75e75e3
Heider <heiderlopez123@gmail.com>
2022-08-02 21:43:42 -0500

    modelo/generos(generos, porque puse animne y es manga)

--- src/models/Generos.ts
+++ src/models/Generos.ts
@@ -5,7 +5,7 @@ import { LibrosGeneros } from "./LibrosGeneros"
 
 @Table
 export class Generos extends Model<iGeneros>{
-    @Column(DataType.ENUM('arte', 'anime', 'biografía', 'biología', 'comic', 'comida', 'computación', 'deporte', 'derecho', 'economía', 'estudio', 'ficción', 'historia', 'humor', 'infantil', 'juvenil', 'matemática', 'medicina', 'novela', 'ocio - tiempo libre', 'política', 'salud - desarrollo personal', 'tecnología', 'terror'))
+    @Column(DataType.ENUM('arte', 'biografía', 'biología', 'comic', 'comida', 'computación', 'deporte', 'derecho', 'economía', 'estudio', 'ficción', 'historia', 'humor', 'infantil', 'juvenil', 'manga', 'matemática', 'medicina', 'novela', 'ocio - tiempo libre', 'política', 'salud - desarrollo personal', 'tecnología', 'terror'))
     genre!: string
 
     @BelongsToMany(() => Libros, () => LibrosGeneros)
