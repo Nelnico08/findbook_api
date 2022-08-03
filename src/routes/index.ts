8bdf87986b2bd684eb6d6b4c5ff12eeac18cf1a6
rmauriciocastilla <rmauriciocastilla@gmail.com>
2022-08-02 23:42:59 -0500

    Tarea routes-getBooks(Endpoint para traer todos los libros)

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,5 +4,9 @@ import { getGenres } from '../controllers/Genre';
 
 export const router = Router();
 
+
 router.get('/books', getBooks);
+// router.post('/books',postBooks);
+
 router.get('/genres', getGenres);
+
