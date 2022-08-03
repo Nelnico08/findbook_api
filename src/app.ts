10bb589ec69bb936ca986959ab809f5c63a46ef6
Nelnico <nelnico008@gmail.com>
2022-08-03 00:33:07 -0300

    factorizacion de documentos(getBooks movido a index de routes)

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,19 @@
 import express, { Application, Request, Response, NextFunction } from 'express';
 import { router } from './routes';
-import booksRouter from './routes/booksRouter';
 const app: Application = express();
 
-app.use(express.json())
+app.use(express.json());
 app.use((req: Request, res: Response, next: NextFunction) => {
-    res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept'
-    );
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-  });
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 
-app.use('/',booksRouter);
 app.use('/', router);
 
 export default app;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express';
-
+import { getBooks } from '../controllers/booksController';
 import { getGenres } from '../controllers/Genre';
 
 export const router = Router();
 
+router.get('/books', getBooks);
 router.get('/genres', getGenres);
