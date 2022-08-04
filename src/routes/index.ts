5bc93ba619c19006a79b5167fbb3df417a98da98
Nelnico <nelnico008@gmail.com>
2022-08-04 12:57:30 -0300

    add deleteBook controller(controllers/deleteBook)

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,14 @@ import { Router } from 'express';
 import { getBookById, getBooks } from '../controllers/GetBooks';
 import { getGenres } from '../controllers/Genre';
 import { postBooks, postGenre } from '../controllers/CargarDb';
+import { deleteBook } from '../controllers/deleteBook';
 
 export const router = Router();
 
 router.get('/books', getBooks);
 router.get('/books/:id', getBookById);
-router.post('/cargardb/books', postBooks);
 router.get('/genres', getGenres);
+router.delete('books/:id', deleteBook);
+
+router.post('/cargardb/books', postBooks);
 router.post('/cargardb/generos', postGenre);
