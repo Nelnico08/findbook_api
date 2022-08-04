a187b9afd2976090fd521523d3d7e74158175a9a
Nelnico <nelnico008@gmail.com>
2022-08-03 21:18:08 -0300

    minor changes(types/generos y models.generos los enum q se vean en pantalla)

--- src/models/Generos.ts
+++ src/models/Generos.ts
@@ -1,13 +1,46 @@
-import { Table, Model, Column, DataType, BelongsToMany } from "sequelize-typescript"
-import { iGeneros } from "../types/Generos"
-import { Libros } from "./Libros"
-import { LibrosGeneros } from "./LibrosGeneros"
+import {
+  Table,
+  Model,
+  Column,
+  DataType,
+  BelongsToMany,
+} from 'sequelize-typescript';
+import { iGeneros } from '../types/Generos';
+import { Libros } from './Libros';
+import { LibrosGeneros } from './LibrosGeneros';
 
 @Table
-export class Generos extends Model<iGeneros>{
-    @Column(DataType.ENUM('arte', 'biografía', 'biología', 'comic', 'comida', 'computación', 'deporte', 'derecho', 'economía', 'estudio', 'ficción', 'historia', 'humor', 'infantil', 'juvenil', 'manga', 'matemática', 'medicina', 'novela', 'ocio - tiempo libre', 'política', 'salud - desarrollo personal', 'tecnología', 'terror'))
-    genre!: string
+export class Generos extends Model<iGeneros> {
+  @Column(
+    DataType.ENUM(
+      'arte',
+      'biografía',
+      'biología',
+      'comic',
+      'comida',
+      'computación',
+      'deporte',
+      'derecho',
+      'economía',
+      'estudio',
+      'ficción',
+      'historia',
+      'humor',
+      'infantil',
+      'juvenil',
+      'manga',
+      'matemática',
+      'medicina',
+      'novela',
+      'ocio - tiempo libre',
+      'política',
+      'salud - desarrollo personal',
+      'tecnología',
+      'terror'
+    )
+  )
+  genre!: string;
 
-    @BelongsToMany(() => Libros, () => LibrosGeneros)
-    libros!: Libros[]
+  @BelongsToMany(() => Libros, () => LibrosGeneros)
+  libros!: Libros[];
 }
