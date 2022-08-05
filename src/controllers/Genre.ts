55ed4ab74279ac3df5b9619368090a7ca1f41b3e
Nelnico <nelnico008@gmail.com>
2022-08-05 17:06:42 -0300

    add pagination to filter by genre and changed force to false(controllers/genre & ./index)

--- src/controllers/Genre.ts
+++ src/controllers/Genre.ts
@@ -2,23 +2,51 @@ import { NextFunction, Request, Response } from 'express';
 import { Generos } from '../models/Generos';
 import { Libros } from '../models/Libros';
 import { genre } from '../types/Generos';
-//prettier-ignore
-export const getGenres = async (req: Request, res: Response, next: NextFunction) => {
+
+export const getGenres = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const genre = req.query.genre as genre;
+
+  const { page, size } = req.query;
+  const pageAsNumber = page ? Number(page) : 1;
+  const sizeAsNumber = size ? Number(size) : 10;
+  let currentPage = 0;
+  let contentSize = 10;
+
+  if (!Number.isNaN(pageAsNumber) && pageAsNumber > 0) {
+    currentPage = pageAsNumber - 1;
+  }
+  if (!Number.isNaN(sizeAsNumber) && sizeAsNumber > 0) {
+    contentSize = sizeAsNumber;
+  }
   try {
-    if(!genre){
+    if (!genre) {
       const generos = await Generos.findAll();
-      return res.json(generos.length?generos:"No hay generos")
+      return res.json(generos.length ? generos : 'Base de datos vacia');
     }
-    const filterGenre = await Generos.findOne({
+    const filterGenre = await Generos.findAndCountAll({
       where: {
-        genre
+        genre,
       },
       include: {
         model: Libros,
-      }
-    })
-    res.json(filterGenre?.genre ? filterGenre : `No existe el genero ${genre}`)
+      },
+      distinct: true,
+      limit: contentSize,
+      offset: currentPage * contentSize,
+    });
+    res.json(
+      filterGenre.rows.length
+        ? {
+            content: filterGenre.rows,
+            totalBooks: filterGenre.count,
+            maxPages: Math.ceil(filterGenre.count / contentSize),
+          }
+        : { message: `No hay libros con el genero ${genre}.` }
+    );
   } catch (err) {
     next(err);
   }
