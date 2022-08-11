63433e5e570baeca7c7d89473aca01c6676abd28
Nelnico <nelnico008@gmail.com>
2022-08-11 19:16:53 -0300

    add an if before load books(controllers/CargarDb) NelsonBranch

--- src/controllers/CargarDb.ts
+++ src/controllers/CargarDb.ts
@@ -35,60 +35,65 @@ export const postBooks = async (
   next: NextFunction
 ) => {
   try {
-    const myDBBooks = await Libros.findAll();
-    if(!myDBBooks.length){ 
-    const books: iLibros[] = mockdata as iLibros[];
-    await Libros.bulkCreate(
-      books.map((b: any) => {
-        return {
-          name: b.name,
-          author: b.author,
-          category: b.category,
-          pages: b.pages,
-          publisher: b.publisher,
-          description: b.description,
-          image: b.image,
-          rating: b.rating,
-          price: b.price,
-          released: b.released,
-          language: b.language,
-        };
-      })
-    );
-    const BooksId = await (await Libros.findAll({ attributes: ['id'] })).sort(function (a, b) {
-      if (a.id > b.id) {
-        return 1;
-      }
-      if (a.id < b.id) {
-        return -1;
-      }
-      // a must be equal to b
-      return 0; 
-    });
-
-    const getGenres = books.map((b: any) =>
-      b.genre.map((g: any) =>
-        Generos.findOne({ where: { genre: g }, attributes: ['id'] })
-      )
-    );
-  
-    let arr = [] as any[];
-    for (let i = 0; i < getGenres.length; i++) {
-      await Promise.all(getGenres[i]).then((r) => arr.push(r));
-    }
-
-    for (let i = 0; i < BooksId.length; i++) {
-      for (let j = 0; j < arr[i].length; j++) {
-        await LibrosGeneros.create({
-          libroid: BooksId[i].id,
-          generoid: arr[i][j].id,
+    const genreDb = await Generos.findAll();
+    if(!genreDb){
+      const myDBBooks = await Libros.findAll();
+      if(!myDBBooks.length){ 
+        const books: iLibros[] = mockdata as iLibros[];
+        await Libros.bulkCreate(
+          books.map((b: any) => {
+            return {
+              name: b.name,
+              author: b.author,
+              category: b.category,
+              pages: b.pages,
+              publisher: b.publisher,
+              description: b.description,
+              image: b.image,
+              rating: b.rating,
+              price: b.price,
+              released: b.released,
+              language: b.language,
+            };
+          })
+        );
+        const BooksId = await (await Libros.findAll({ attributes: ['id'] })).sort(function (a, b) {
+          if (a.id > b.id) {
+            return 1;
+          }
+          if (a.id < b.id) {
+            return -1;
+          }
+          // a must be equal to b
+          return 0; 
         });
+
+        const getGenres = books.map((b: any) =>
+          b.genre.map((g: any) =>
+        Generos.findOne({ where: { genre: g }, attributes: ['id'] })
+        )
+      );
+  
+      let arr = [] as any[];
+      for (let i = 0; i < getGenres.length; i++) {
+        await Promise.all(getGenres[i]).then((r) => arr.push(r));
       }
+
+      for (let i = 0; i < BooksId.length; i++) {
+        for (let j = 0; j < arr[i].length; j++) {
+          await LibrosGeneros.create({
+            libroid: BooksId[i].id,
+            generoid: arr[i][j].id,
+          });
+        }
+      }
+      return res.json('DataBase Cargada');
+      }else{
+      return res.json('La DataBase Ya Esta Cargada');
+      }
+    }else{
+      return res.json("Cargar con generos la base de datos primero")
     }
-    return res.json('DataBase Cargada');
-  }else{
-    return res.json('La DataBase Ya Esta Cargada');
-  }
   } catch (error) {
     next(error);
   }
