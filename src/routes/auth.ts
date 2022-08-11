3de2c74e18073e416f4588b90fef05bac64d7f66
rmauriciocastilla <rmauriciocastilla@gmail.com>
2022-08-11 17:49:53 -0500

    fix cargarDB and (middlewares with admin routes)

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
+import { isAdmin } from "../authentication/authAdmin";
 import { registerUser,loginUser } from "../authentication/authLogRes";
+import { verifyToken } from "../middlewares/verifyToken";
 const router = Router();
 
 router.post('/register',registerUser);
 router.post('/login',loginUser);
+router.get('/admin',[verifyToken],isAdmin);
 
 export default router;
