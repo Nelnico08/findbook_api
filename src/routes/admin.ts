49408e30f7c004f1d700c90ff8ae1d88ae53f58d
Heider <heiderlopez123@gmail.com>
2022-08-12 07:57:42 -0500

    getUsers(ruta para que el admin pueda traer y ver a todos los usuarios)

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { getUsers } from '../admin/GetUsers';
 import { putUserAdmin } from '../admin/PutUserAdmin';
 import { putBookAdmin } from '../admin/PutBookAdmin';
 import { deleteUserAdmin } from '../admin/DeleteUserAdmin';
@@ -7,6 +8,7 @@ import { verifyToken } from '../middlewares/verifyToken';
 import { verifyAdmin } from '../middlewares/verifyAdmin';
 const router = Router();
 
+router.put('/users', [verifyToken, verifyAdmin], getUsers)
 router.put('/:id', [verifyToken, verifyAdmin], putUserAdmin)
 router.put('/:id', [verifyToken, verifyAdmin], putBookAdmin)
 router.delete('/:id', [verifyToken, verifyAdmin], deleteUserAdmin)
