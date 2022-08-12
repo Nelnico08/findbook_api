8409305f329ea6cc5419674ffec63d7b274c5819
rmauriciocastilla <rmauriciocastilla@gmail.com>
2022-08-11 20:38:05 -0500

    Autenticacion admin/deletebook(Agregamos la autenticacion del administrados a la ruta deleteBook)

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import { deleteBookAdmin } from '../admin/DeleteBookAdmin';
-
+import { verifyToken } from '../middlewares/verifyToken';
+import { verifyAdmin } from '../middlewares/verifyAdmin';
 const router = Router();
 
-router.delete('/:id', deleteBookAdmin);
+router.delete('/:id', [verifyToken,verifyAdmin],deleteBookAdmin);
 
 export default router;
