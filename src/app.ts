6276187b42dc1aaea142696458d75d27b0e44bfc
Nelnico <nelnico008@gmail.com>
2022-08-12 00:44:16 -0300

    deleted console log in verify token(middleware/verifyToken) NelsonBranch

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,17 @@
-import express, { Application, Request, Response, NextFunction } from 'express';
+import express, { Application } from 'express';
 import { router } from './routes';
+const cors = require('cors')
 const app: Application = express();
 
 app.use(express.json());
-app.use((req: Request, res: Response, next: NextFunction) => {
-  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-});
+app.use(
+  cors({
+   origin: "*",
+   credentials: true,
+   methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'DELETE'],
+   allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization', 'authorization'],
+  }),
+ );
 
 app.use('/', router);
 
