20f8e76fbf74246f8d6d954a5187d6cf2e009313
NurCord <nunyypin@gmail.com>
2022-08-17 18:12:09 -0300

    feat(src/admin & arc/user) NuriaBranch arregle admin de nuevo

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -7,12 +7,14 @@ import { verifyToken } from '../middlewares/verifyToken';
 import { verifyAdmin } from '../middlewares/verifyAdmin';
 import { getUser } from '../controllers/admin/GetUser';
 import { getBooks } from '../controllers/user/GetBooks';
+import { putUserAdmin } from '../controllers/admin/PutUserAdmin'
 const router = Router();
 
 router.get('/users', [verifyToken, verifyAdmin], getUsers)
 router.get('/users/:username', [verifyToken, verifyAdmin], getUser)
 router.get('/books', [verifyToken, verifyAdmin], getBooks)
 router.put('/putbook/:id', [verifyToken, verifyAdmin], putBookAdmin)
+router.put('/putuser/:email', [verifyToken, verifyAdmin], putUserAdmin)
 router.delete('/deleteuser/:email', [verifyToken, verifyAdmin], deleteUserAdmin)
 router.delete('/deletebook/:id', [verifyToken, verifyAdmin], deleteBookAdmin);
 
