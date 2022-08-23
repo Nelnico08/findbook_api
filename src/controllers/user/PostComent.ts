f5fc091180f9362f72beebd2e5ef04e071a18bc8
Nelnico <nelnico008@gmail.com>
2022-08-22 23:45:31 -0300

    added sendMail to postComment. NelsonBranch

--- src/controllers/user/PostComent.ts
+++ src/controllers/user/PostComent.ts
@@ -1,6 +1,10 @@
 import { NextFunction, Request, Response } from "express"
 import { iComentarios } from "../../types/Comentarios"
 import { Comentarios } from '../../models/Comentarios'
+import { Libros } from "../../models/Libros"
+import { Usuario } from "../../models/Usuario"
+import { sendEmail } from "../../utils/email"
+import { comment } from "../../utils/messages"
 
 export const PostComent = async (req: Request, res: Response, next: NextFunction) => {
     const user_id = req.user_id
@@ -14,6 +18,19 @@ export const PostComent = async (req: Request, res: Response, next: NextFunction
             libroid: book_id,
             usuarioid: user_id
         })
+        const userComment = await Usuario.findByPk(user_id)
+        const book = await Libros.findOne({
+            where:{id:book_id},
+            include:{
+                model:Usuario,
+                attributes:['id','name','email']
+            }
+        });
+        if(book && book.usuario.id !== user_id && userComment){
+            const subject= "Comentario en una de tus publicaciones";
+            const message= comment.replace("{USER}", `${book.usuario.name}`).replace("{BOOK}", `${book.name}`).replace("{COMENTARIO}",`${comentario}`);
+            sendEmail(book.usuario.email, book.usuario.name, message, subject)
+        }
         res.json('Comentario agregado correctamente')
     } catch (error) {
         next(error)
