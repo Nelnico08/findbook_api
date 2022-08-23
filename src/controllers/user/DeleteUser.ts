224af3793fd305a71ad6dbee4ab346be7345465f
rmauriciocastilla <rmauriciocastilla@gmail.com>
2022-08-22 23:03:18 -0500

    Fixing delete account

--- src/controllers/user/DeleteUser.ts
+++ src/controllers/user/DeleteUser.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from 'express';
 import { Usuario } from '../../models/Usuario';
 
+const bcrypt = require('bcrypt');
+
 export const deleteUser = async (
     req: Request,
     res: Response,
@@ -9,16 +11,19 @@ export const deleteUser = async (
     try {
         const userid = req.user_id;
         const user = await Usuario.findByPk(userid);
+        const {password} = req.body;
         if(!user){
             return res.json({error: 'Usuario inexistente'});
         }
+        const comparePass = await bcrypt.compare(password,user.password)
+        if(!comparePass){
+            return res.json({error: "La constraseña es incorrecta"});
+        }
         await user.update({
-            status: 'deleted'   
-        });
+            status: 'deleted'
+        })
         return res.json('La cuenta ha sido eliminada correctamente')
     } catch (error) {
         return res.json({error: 'Ha ocurrido un error, intentelo mas tarde'});
     }
-    
-
 }
