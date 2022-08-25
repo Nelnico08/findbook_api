3697da6d5c1c95c771867ec57e8da0fd02b2e003
Nelnico <nelnico008@gmail.com>
2022-08-25 11:51:47 -0300

    changed findbook Email

--- src/utils/email.ts
+++ src/utils/email.ts
@@ -15,7 +15,7 @@ export let sendEmail = async (email: string, name:string, message: string, subje
             Messages: [
                 {
                     From: {
-                    Email: "customerfindbook@gmail.com",
+                    Email: "customerfindbook@findbook.com",
                     Name: "Findbook"
                     },
                     To: [
